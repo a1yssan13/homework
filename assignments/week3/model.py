40367e93d692110bfef4e17068cb17577b380c52
a1yssan13 <niealyssa@gmail.com>
2023-02-07 21:19:37 -0500

    finish

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,8 +1,9 @@
 import torch
+import torch.nn as nn 
 from typing import Callable
 
 
-class MLP:
+class MLP(nn.Module):
     def __init__(
         self,
         input_size: int,
@@ -22,7 +23,15 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+        self.act = activation()
+        self.first = nn.Linear(input_size, hidden_size)
+        self.hidden_layers = nn.ModuleList([nn.Linear(hidden_size, hidden_size) for _ in range(hidden_count)])
+        self.last = nn.Linear(hidden_size, num_classes)
+        initializer(self.first.weight) 
+        for layer in self.hidden_layers:
+            initializer(layer.weight) 
+        initializer(self.last.weight)
 
     def forward(self, x):
         """
@@ -34,4 +43,8 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        x = self.act(self.first(x))
+        for layer in self.hidden_layers:
+            x = self.act(layer(x)) 
+        x = self.last(x) 
+        return x 
