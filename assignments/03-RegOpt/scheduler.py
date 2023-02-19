b36d120e0b7bf3fedc8d96cd641bf84dcf10a115
a1yssan13 <niealyssa@gmail.com>
2023-02-19 08:40:25 -0500

    submission 1

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,24 +1,26 @@
 from typing import List
+import math
 
 from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
-        """
-        Create a new scheduler.
+    """
+    A custom learning rate scheduler that inherits from the PyTorch _LRScheduler module.
+    """
 
-        Note to students: You can change the arguments to this constructor,
-        if you need to add new parameters.
-
-        """
-        # ... Your Code Here ...
+    def __init__(self, optimizer, T_max, eta_min=0, last_epoch=-1):
+        self.T_max = T_max
+        self.eta_min = eta_min
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
-        # Note to students: You CANNOT change the arguments or return type of
-        # this function (because it is called internally by Torch)
+        """
+        Compute the learning rate at the current epoch.
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        Returns:
+            List[float]: A list of learning rates, one for each parameter group in the optimizer.
+        """
+        return [self.eta_min + (base_lr - self.eta_min) *
+                (1 + math.cos(math.pi * self.last_epoch / self.T_max)) / 2
+                for base_lr in self.base_lrs]
