5d2d6687141fe0ff4bfa7425464628a1955c31da
a1yssan13 <niealyssa@gmail.com>
2023-03-18 14:23:08 -0400

    first sub

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,15 +2,41 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
+from torchvision import transforms
 
 
 class CONFIG:
-    batch_size = 8
-    num_epochs = 8
+    batch_size = 64 * 8
+    num_epochs = 2
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=0.008)
 
-    transforms = Compose([ToTensor()])
+    # optimizer_factory: Callable[
+    #     [nn.Module], torch.optim.Optimizer
+    # ] = lambda model: torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    # optimizer_factory: Callable[
+    #     [nn.Module], torch.optim.Optimizer
+    # ] = lambda model: torch.optim.AdamW(model.parameters(), lr=3e-4)
+
+    transforms = Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            # transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+            ToTensor(),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+    # transforms = Compose(
+    #     [
+    #         # transforms.RandomHorizontalFlip(),
+    #         # transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.9, 1.1)),
+    #         transforms.ToTensor(),
+    #         transforms.Normalize(
+    #             [0.49139968, 0.48215841, 0.44653091],
+    #             [0.24703223, 0.24348513, 0.26158784],
+    #         ),
+    #     ]
+    # )
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,42 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
+    """
+    CIFAR - 10 classification model made with ConvNet
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.bn1 = nn.BatchNorm2d(6)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+        self.dropout = nn.Dropout()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """
+        Performs a forward pass through the neural network.
+        Args:
+            x (torch.Tensor): A tensor of shape (batch_size, input_size) representing the input data.
+        Returns:
+            torch.Tensor: A tensor of shape (batch_size, 10) representing classifcations
+        """
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.bn1(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.bn2(x)
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        # x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        # x = self.dropout(x)
+        x = self.fc3(x)
+        return x
